053f55e97af31a8a7503f0b6857631d32028ef8d
fizzfau <talhahasimosmanoglu@gmail.com>
2023-11-04 12:33:43 +0300

    increment

--- routes/increment.js
+++ routes/increment.js
@@ -8,13 +8,16 @@ router.post('/incrementCredit', (req, res) => {
     CoffeeCredits.findOne({userId: req.body.id})
     .exec()
     .then(user => {
+        req.body.credits = parseInt(req.body.credits);
         user.credits += req.body.credits;
+        console.log(11, user.credits, req.body.credits);
         if (user.credits >= 10) {
+            console.log(14)
             user.coffee += 1;
             user.credits = 0;
         }
         user.save();
-        res.json({
+        res.status(200).json({
             credits: user.credits,
             coffee: user.coffee,
             name: req.user.name,
@@ -22,7 +25,7 @@ router.post('/incrementCredit', (req, res) => {
         })
     })
     .catch(err => {
-        res.json({
+        res.status(401).json({
             message: err
         })
     })
