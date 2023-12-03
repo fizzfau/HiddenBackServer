3068390963289675769be5f1ba5a7d5d14bb194d
fizzfau <talhahasimosmanoglu@gmail.com>
2023-12-03 13:50:58 +0300

    register + login confirm update

--- Authentication/AuthController.js
+++ Authentication/AuthController.js
@@ -1,112 +1,140 @@
 const User = require('../models/User');
+const Cooperative = require('../models/Cooperative');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
-const register = async (req, res) => {
-    console.log(12, req.body)
-    // bcrypt.hash(req.body.password, 10, async (err, hashedPass) => {
-    //     if (err) {
-    //         res.json({
-    //             error: err
-    //         })
-    //     }
-    //     const userId = await createUniqueID();
-    //     User.find({
-    //         plate: req.body.plate
-    //     }).exec().then(user => {
-    //         if (user.length >= 1) {
-    //             return res.status(200).json({
-    //                 message: "Plaka zaten kayıtlı!"
-    //             });
-    //         } else {
-    //             let user = new User({
-    //                 name: req.body.name,
-    //                 plate: req.body.plate,
-    //                 coopId: req.body.coopId,
-    //                 userId: userId,
-    //                 password: hashedPass
-    //             })
-    //             try {
-    //                 const savedUser = user.save();
-    //                 res.json(savedUser);
-    //             } catch (err) {
-    //                 res.json({
-    //                     message: err
-    //                 })
-    //             }
-    //         }
-    //     })
-    // })
+const register =  (req, res) => {
+    
+    bcrypt.hash(req.body.password, 10, async (err, hashedPass) => {
+        if (err) {
+            res.json({
+                error: err
+            })
+        }
+        const userId = await createUniqueID();
+
+        Cooperative.findOne({
+            cooperativeId: req.body.coopId
+        }).exec().then(coop => {
+            if (!coop) {
+                return res.status(200).json({
+                    success: false,
+                    message: "Kooperatif bulunamadı!"
+                });
+            } else {
+                User.find({
+                    plate: req.body.plate
+                }).exec().then(user => {
+                    if (user.length >= 1) {
+                        return res.status(200).json({
+                            success: false,
+                            message: "Plaka zaten kayıtlı!"
+                        });
+                    } else {
+                        let user = new User({
+                            name: req.body.name,
+                            plate: req.body.plate,
+                            coopId: req.body.coopId,
+                            userId: userId,
+                            password: hashedPass
+                        })
+                        try {
+                            const savedUser = user.save();
+                            if (!savedUser) throw Error('Something went wrong saving the user');
+                            res.status(200).json({
+                                success: true
+                            });
+                        } catch (err) {
+                            res.status(200).json({
+                                success: false,
+                                message: err.message
+                            });
+                        }
+                    }
+                })
+            }
+
+        })
+
+
+    })
 }
 
 const login = async (req, res) => {
     const plate = req.body.plate.toString();
     const password = req.body.password;
-    console.log(45, plate, password)
+
     User.find({
-            plate: plate
-        })
+        plate: plate
+    })
         .exec()
         .then(user => {
             if (user.length < 1) {
-                return res.status(401).json({
-                    message: "User does not found!"
+                return res.status(200).json({
+                    success: false,
+                    message: "Kayıtlı plaka bulunamadı!"
                 });
             }
             bcrypt.compare(password, user[0].password, (err, result) => {
                 if (err) {
-                    return res.status(401).json({
+                    return res.status(200).json({
+                        success: false,
                         message: "Authentication failed!"
                     });
                 }
                 if (result) {
+                    if (!user[0].confirmed) {
+                        return res.status(200).json({
+                            success: false,
+                            message: "Hesabınız henüz onaylanmamış!"
+                        });
+                    }
+
                     const token = jwt.sign({
-                            name: user[0].name,
-                            userId: user[0].userId,
-                            plate: user[0].plate,
-                            coopId: user[0].coopId
-                        },
+                        name: user[0].name,
+                        userId: user[0].userId,
+                        plate: user[0].plate,
+                        coopId: user[0].coopId
+                    },
                         process.env.SECRET, {
-                            expiresIn: "99y"
-                        }
+                        expiresIn: "99y"
+                    }
                     );
-                    console.log(71, user)
+
                     return res.status(200).json({
+                        success: true,
                         message: "Authentication successful",
                         token: token,
                         user: {
                             name: user[0].name,
                             userId: user[0].userId,
                             plate: user[0].plate,
-                            coopId: user[0].coopId   
+                            coopId: user[0].coopId
                         },
                     });
                 } else {
-                    return res.status(401).json({
-                        message: "Password does not match!"
+                    return res.status(200).json({
+                        success: false,
+                        message: "Şifre hatalı!"
                     });
                 }
             });
         }
-    );
-}
-
-function generateSixDigitRandom() {
-    return Math.floor(100000 + Math.random() * 900000); // Generates a random 6-digit number
+        );
 }
 
 async function createUniqueID() {
     let isUnique = false;
     let newUniqueId;
-    
+
     while (!isUnique) {
-      newUniqueId = generateSixDigitRandom();
-      // Check if the generated ID already exists in the database
-      const existingDoc = await User.findOne({ userId: newUniqueId });
-      if (!existingDoc) {
-        isUnique = true;
-      }
+        newUniqueId = Math.floor(100000 + Math.random() * 900000);
+        // Check if the generated ID already exists in the database
+        const existingDoc = await User.findOne({ userId: newUniqueId });
+        if (!existingDoc) {
+            isUnique = true;
+        }
     }
     console.log(`New unique ID generated: ${newUniqueId}`);
     return newUniqueId.toString();
