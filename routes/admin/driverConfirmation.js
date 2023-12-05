49a59c12e7745e688a1094065b2452ba64a4cb1b
fizzfau <talhahasimosmanoglu@gmail.com>
2023-12-06 01:26:12 +0300

    add/remove drivers to/from queue

--- routes/admin/driverConfirmation.js
+++ routes/admin/driverConfirmation.js
@@ -1,6 +1,7 @@
 const User = require('../../models/User');
 const router = require('express').Router();
 const authenticate = require('../../middleware/authenticate');
+const Cooperative = require('../../models/Cooperative');
 
 router.get("/getUnconfirmedDrivers", authenticate, function(req, res) {
     const coopId = req.user.coopId;
@@ -23,6 +24,16 @@ router.post("/confirmDriver", authenticate, function(req, res) {
     }
     User.findOneAndUpdate({ coopId: coopId, userId: userId }, { confirmed: true }).exec()
     .then(user => {
+        // add user to coopDriverQueue
+        Cooperative.findOne({ coopId: coopId }).exec()
+        .then(coop => {
+            let queue = JSON.parse(coop.coopDriverQueue);
+            queue.push(user.userId);
+            Cooperative.findOneAndUpdate({ coopId: coopId }, { coopDriverQueue: JSON.stringify(queue) }).exec()
+            .then(() => {
+                res.status(200).send({ success: true, user });
+            })
+        })
         res.status(200).send({ success: true });
     }).catch(err => {
         res.status(200).send({ success: false, message: err.message });
--- routes/drivers/deleteAccount.js
+++ routes/drivers/deleteAccount.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
+const Cooperative = require('../../models/Cooperative');
 const Jobs = require('../../models/Jobs');
 const authenticate = require('../../middleware/authenticate');
 
@@ -11,6 +12,25 @@ router.delete('/deleteAccount', authenticate, (req, res) => {
         Jobs.deleteMany({job_driver_id: req.user.userId})
         .exec()
         .then(jobs => {
+            Cooperative.findOne({coopId: req.user.coopId})
+            .exec()
+            .then(coop => {
+                let queue = JSON.parse(coop.coopDriverQueue);
+                let index = queue.indexOf(req.user.userId);
+                queue.splice(index, 1);
+                Cooperative.findOneAndUpdate({coopId: req.user.coopId}, {coopDriverQueue: JSON.stringify(queue)})
+                    .exec()
+                    .then(() => {
+                        res.status(200).json({
+                            message: 'Hesabınız başarıyla silindi.'
+                        })
+                    })
+                    .catch(err => {
+                        res.json({
+                            message: err
+                        })
+                    })
+                })
             res.status(200).json({
                 message: 'Hesabınız başarıyla silindi.'
             })
